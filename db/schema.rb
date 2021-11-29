b1f3cfd56b65f83f51f5561a1b33c74faba1a997
John Nkp <john.nkp1@gmail.com>
2021-11-29 22:38:50 +1100

    added notes table to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_19_111053) do
+ActiveRecord::Schema.define(version: 2021_11_29_111234) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "notes", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_notes_on_user_id"
+  end
 
   create_table "subtasks", force: :cascade do |t|
     t.string "name"
@@ -50,6 +59,7 @@ ActiveRecord::Schema.define(version: 2021_11_19_111053) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "notes", "users"
   add_foreign_key "subtasks", "tasks"
   add_foreign_key "tasks", "users"
 end
