12cd5fa4357fad461b60a760dd28e47c541039b5
John Nkp <john.nkp1@gmail.com>
2021-11-30 22:31:04 +1100

    task controller reconfigured

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,8 +2,8 @@ class Task < ApplicationRecord
     # has_one :user
     has_many :subtask
     # belongs_to :user, :class_name => 'User', :foreign_key => 'user_id'
-    # belongs_to :user, default: -> { Current.user }, :foreign_key => 'user_id'
-    belongs_to :user
+    belongs_to :user, default: -> { Current.user }, :foreign_key => 'user_id'
+    # belongs_to :user
     has_one :user_task
     
 end
