2c832d5c0a07df92b613b8bec7b24ab5d7b0a37a
John Nkp <john.nkp1@gmail.com>
2021-11-30 20:16:55 +1100

    modified database

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-    # before_action :authorized
+    before_action :authorized
     before_action :set_task, only: [:show, :update, :destroy]
 
     def index 
