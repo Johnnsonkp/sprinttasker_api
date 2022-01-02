ffdad0a3dd755059f9add983d34d9e94dcdd47e1
John Nkp <john.nkp1@gmail.com>
2022-01-02 19:58:11 +1100

    db updated

--- db/migrate/20220102071747_add_subtask_to_task.rb
+++ db/migrate/20220102071747_add_subtask_to_task.rb
@@ -1,4 +1,7 @@
 class AddSubtaskToTask < ActiveRecord::Migration[6.1]
+  # def change
+  #   add_reference :tasks, :subtask, foreign_key: true
+  # end
   def change
     add_column :tasks, :subtask, :string
   end
