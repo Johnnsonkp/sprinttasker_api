adc444c10a2e5717c2098d4236b39ec8bc85f2de
John Nkp <john.nkp1@gmail.com>
2022-01-13 07:32:45 +1100

    minor tweaks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_12_101947) do
+ActiveRecord::Schema.define(version: 2022_01_12_202958) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2022_01_12_101947) do
     t.text "description"
     t.string "subtask"
     t.string "timer"
+    t.string "time_to_complete"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
