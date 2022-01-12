145f06b777b211f18c6e6c5970d7e8526231cc02
John Nkp <john.nkp1@gmail.com>
2022-01-12 21:46:11 +1100

    default timer set 2

--- db/migrate/20220112101947_add_timer_to_tasks.rb
+++ db/migrate/20220112101947_add_timer_to_tasks.rb
@@ -1,5 +1,5 @@
 class AddTimerToTasks < ActiveRecord::Migration[6.1]
   def change
-    add_column :tasks, :timer, :string, :default => "P"
+    add_column :tasks, :timer, :string, :default => "00:00"
   end
 end
