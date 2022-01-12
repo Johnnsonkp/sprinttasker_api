ef6c8d6f65430b0daad445b3fb9fef76d96311fa
John Nkp <john.nkp1@gmail.com>
2022-01-12 21:38:45 +1100

    set default value for timer

--- db/migrate/20220102071747_add_subtask_to_task.rb
+++ db/migrate/20220102071747_add_subtask_to_task.rb
@@ -3,6 +3,6 @@ class AddSubtaskToTask < ActiveRecord::Migration[6.1]
   #   add_reference :tasks, :subtask, foreign_key: true
   # end
   def change
-    add_column :tasks, :subtask, :string
+    add_column :tasks, :subtask, :string, :default => "00:00"
   end
 end
