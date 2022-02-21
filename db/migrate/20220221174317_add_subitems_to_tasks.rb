2b9bc9e21918087ebe4538e96e24d19b3d1fec91
John Nkp <john.nkp1@gmail.com>
2022-02-22 04:49:00 +1100

    changed subitems data type to array of strings

--- db/migrate/20220221174317_add_subitems_to_tasks.rb
+++ db/migrate/20220221174317_add_subitems_to_tasks.rb
@@ -1,5 +1,5 @@
 class AddSubitemsToTasks < ActiveRecord::Migration[6.1]
   def change
-    add_column :tasks, :subitems, :array
+    add_column :tasks, :subitems, :string, array: true, default: []
   end
 end
