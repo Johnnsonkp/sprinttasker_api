95e2df30ff4358863764284d8b9f3a5ee71e8dd2
John Nkp <john.nkp1@gmail.com>
2022-02-22 04:23:10 +1100

    order added to task table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_12_202958) do
+ActiveRecord::Schema.define(version: 2022_02_21_163706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,9 +40,10 @@ ActiveRecord::Schema.define(version: 2022_01_12_202958) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id"
     t.text "description"
-    t.string "subtask"
     t.string "timer"
     t.string "time_to_complete"
+    t.string "subtask", array: true
+    t.integer "order"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
